90d6337a5b88f5a59411fad65c27f5399aaf9774
Josh Kirby <joki2454@colorado.edu>
2018-10-26 11:50:04 -0600

    populate the first couple functions

---
+++ init.m
@@ -0,0 +1,19 @@
+%% Authors: Joshua Kirby and Amanda Siirola
+%  Created: 10/26/2018
+% Modified: 10/26/2018
+%
+% Purpose: Execute the code for the ASEN 5050 Project.
+%
+%% Housekeeping
+close all;clc
+
+%% Project Settings
+SET = projectSettings();
+
+%% Define Nominal Orbit (No maneuvers at perijove if possible)
+
+
+%% Explore SMA and I arrival solution space (ignore RAAN? that's what I'm thinking)
+
+
+%% Present Results
---
+++ projectSettings.m
@@ -0,0 +1,23 @@
+%% Authors: Joshua Kirby and Amanda Siirola
+%  Created: 10/26/2018
+% Modified: 10/26/2018
+%
+% Purpose: Return execution settings for the ASEN 5050 Project.  Should be
+% passed into and out of every major function in init.m.  Configurables and
+% user settings should be defined here.
+%
+% Inputs:
+% 
+% Outputs: 
+%   SET - struct of project settings
+function SET = projectSettings()
+%% 
+
+
+
+
+
+
+
+
+end
